96eb268e6020d40c921430adcaa3e4e7ee5bd8d8
Ming-Yuan Yu <myyu@umich.edu>
2020-03-29 17:55:02 -0400

    update train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 if [ -z $1 ] ; then
-  GPU=0
+  GPU=all
 else
   GPU=$1
 fi
@@ -10,15 +10,12 @@ mkdir -p $HOME/.keras
 WORKDIR=/mnist_tf
 
 docker run -it --rm \
-  --gpus='"device='$GPU'"' \
-  --name='mnist-gpu-'$GPU \
+  --gpus '"device='$GPU'"' \
+  --name 'mnist-gpu-'$GPU \
   -u $(id -u):$(id -g) \
   -e HOME=$HOME \
   -v $HOME/.keras:$HOME/.keras \
-  -v $PWD/train.py:$WORKDIR/train.py:ro \
-  -v $PWD/util.py:$WORKDIR/util.py:ro \
-  -v $PWD/logs:$WORKDIR/logs \
-  -v $PWD/checkpoints:$WORKDIR/checkpoints \
+  -v $(pwd):$WORKDIR \
   -v /etc/timezone:/etc/timezone:ro \
   -v /etc/localtime:/etc/localtime:ro \
   -w $WORKDIR \
